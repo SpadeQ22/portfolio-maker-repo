2c68b9a51e992e48b4ddeccc5364b8487be38a8b
SpadeQ22 <67244800+SpadeQ22@users.noreply.github.com>
2021-09-12 01:20:33 +0200

    made a dict with course codes and urls

--- RequestMaker/test.py
+++ RequestMaker/test.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from bs4 import BeautifulSoup
 
 app_data = {
     "login": "19p8102@eng.asu.edu.eg",
@@ -17,6 +18,8 @@ def auth_moodle(data: dict) -> requests.Session():
     token = re.findall("\w{32}", token[0])[0]
     payload = {'anchor': '', 'logintoken': token, 'username': login, 'password': password, 'rememberusername': 1}
     r_2 = s.post(url=url_domain + "/login/index.php", data=payload)
+    sessionStr = re.findall('"sesskey":"\w*',r_2.text )
+    sessionkey =( re.split(':', sessionStr[0])[1])[1:]
     for i in r_2.text.splitlines():
         if "<title>" in i:
             print(i[15:-8:])
@@ -26,10 +29,20 @@ def auth_moodle(data: dict) -> requests.Session():
         if "loginerrors" in i or (0 < counter <= 3):
             counter += 1
             print(i)
-    return s
+    return s, sessionkey
 
 
-s = auth_moodle(data=app_data)
-r = s.get("https://lms.eng.asu.edu.eg/my")
+s, sessionkey = auth_moodle(data=app_data)
 
-print(r.text)
+Param = {'sesskey':f'{sessionkey}',
+        'info':'core_course_get_enrolled_courses_by_timeline_classification'}
+
+dict_pay = '[{"index":0,"methodname":"core_course_get_enrolled_courses_by_timeline_classification","args":{"offset":0,"limit":96,"classification":"all","sort":"fullname"}}]'
+r = s.get("https://lms.eng.asu.edu.eg/lib/ajax/service.php",
+          data= dict_pay, params=Param)
+
+data = r.json()
+subjects = {subject['fullname'].split()[0]:subject['viewurl'] for subject in data[0]['data']['courses'] }
+
+
+
