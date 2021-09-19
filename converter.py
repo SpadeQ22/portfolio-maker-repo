b7b10b67771bc2170b7ae33be5396f179bebbc0d
Hussein-Selim <husseinahmedselim@gmail.com>
2021-09-19 17:48:47 +0200

    converter done, requirements folder updated!!!

---
+++ converter.py
@@ -0,0 +1,32 @@
+from docx2pdf import convert
+import os
+from PIL import Image
+
+
+# Configure API key authorization: Apikey
+
+
+def word_to_pdf(input_file):
+    convert(input_file)
+    os.remove(input_file)
+
+
+def img_to_pdf(input_file):
+    img = Image.open(input_file)
+    img = img.convert('RGB')
+    img.save(input_file.rsplit(".", 1)[0] + '.pdf')
+    os.remove(input_file)
+
+
+def convert_to_pdf(input_file):
+    try:
+        if input_file.endswith('docx') or input_file.endswith('doc'):
+            word_to_pdf(input_file)
+        elif input_file.endswith('png') or input_file.endswith('jpg'):
+            img_to_pdf(input_file)
+
+    except IOError:
+        print("file doesn't end with: docx, doc, png or jpg!")
+
+
+convert_to_pdf('Autofill/img.jpg')
